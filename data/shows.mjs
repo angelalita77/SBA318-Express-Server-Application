43821a813109ccb14a633d6948e80df7e4c32608
angelalita77 <anjeanette@gmail.com>
2025-09-13 18:24:24 -0400

    remove 'img' key from show object array in shows.mjs

--- data/shows.mjs
+++ data/shows.mjs
@@ -3,12 +3,10 @@ export const shows = [
     id: 0,
     title: "Fullmetal Alchemist: Brotherhood",
     year: 2009
-    // img: "https://upload.wikimedia.org/wikipedia/en/7/7e/Fullmetal_Alchemist_Brotherhood_key_visual.png"
   },
   {
     id: 1,
     title: "Cowboy Bebop",
     year: 1998
-    // img: "https://upload.wikimedia.org/wikipedia/en/a/a9/Cowboy_Bebop_key_visual.jpg"
   }
 ];
