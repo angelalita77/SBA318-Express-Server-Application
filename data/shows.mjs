d3b719129ef0d758f5534bfc1c23af3dae949ad7
angelalita77 <anjeanette@gmail.com>
2025-09-12 21:52:22 -0400

    fixed and added data collection points

---
+++ data/shows.mjs
@@ -0,0 +1,24 @@
+export const show = [
+    {
+        "id": 0,
+        "show": "Fullmetal Alchemist: Brotherhood",
+        "genre": ["Shounen", "comedy", "fantasy", "thriller"],
+        "episodes": 64,
+        "yearOfRelease": 1995
+    },
+    {
+        "id": 2,
+        "show": "Neon Gensis Evangelion",
+        "genre": ["psycological", "action", "sci-fi"],
+        "episodes": 64,
+        "yearOfRelease": 1995
+    },
+    {
+        "id": 4,
+        "show": "Cowboy Bebop",
+        "genre": ["drama", "action", "sci-fi"],
+        "episodes": 26,
+        "yearOfRelease": 1998
+    }
+
+]
