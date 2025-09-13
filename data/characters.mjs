c9cc3801ca2ac1cc71000348f73029ff472b4cb5
angelalita77 <anjeanette@gmail.com>
2025-09-12 23:28:45 -0400

    fixed the data collections to be more structured

---
+++ data/characters.mjs
@@ -0,0 +1,20 @@
+export const characters = [
+  {
+    id: 0,
+    name: "Roy Mustang",
+    showId: 0,
+    img: "https://www.nicepng.com/png/detail/169-1698839_roy-mustang.png"
+  },
+  {
+    id: 1,
+    name: "Alphonse Elric",
+    showId: 0,
+    img: "https://www.nicepng.com/png/detail/169-1698641_alphonse-elric-fullmetal-alchemist-alphonse-png.png"
+  },
+  {
+    id: 2,
+    name: "Spike Spiegel",
+    showId: 1,
+    img: "https://www.animeemanga.it/wp-content/uploads/2010/11/Spike-Spiegel.jpg"
+  }
+];
--- data/shows.mjs
+++ data/shows.mjs
@@ -1,24 +1,14 @@
-export const show = [
-    {
-        "id": 0,
-        "show": "Fullmetal Alchemist: Brotherhood",
-        "genre": ["Shounen", "comedy", "fantasy", "thriller"],
-        "episodes": 64,
-        "yearOfRelease": 1995
-    },
-    {
-        "id": 2,
-        "show": "Neon Gensis Evangelion",
-        "genre": ["psycological", "action", "sci-fi"],
-        "episodes": 64,
-        "yearOfRelease": 1995
-    },
-    {
-        "id": 4,
-        "show": "Cowboy Bebop",
-        "genre": ["drama", "action", "sci-fi"],
-        "episodes": 26,
-        "yearOfRelease": 1998
-    }
-
-]
+export const shows = [
+  {
+    id: 0,
+    title: "Fullmetal Alchemist: Brotherhood",
+    year: 2009,
+    img: "https://upload.wikimedia.org/wikipedia/en/7/7e/Fullmetal_Alchemist_Brotherhood_key_visual.png"
+  },
+  {
+    id: 1,
+    title: "Cowboy Bebop",
+    year: 1998,
+    img: "https://upload.wikimedia.org/wikipedia/en/a/a9/Cowboy_Bebop_key_visual.jpg"
+  }
+];
